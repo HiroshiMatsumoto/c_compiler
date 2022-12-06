f1a0a4f13a7ce4464bc1463928702be5c8a14a19
matsumoto <h_matsumoto@hotmail.com>
2022-12-06 23:29:32 +0900

    fix

--- 9cc.c
+++ 9cc.c
@@ -5,7 +5,6 @@
 #include <stdarg.h>
 #include <string.h>
 
-
 typedef enum {
   TK_RESERVED, // symbols
   TK_NUM, // integer tokens
@@ -119,6 +118,75 @@ void error_at(char* loc, char* fmt, ...){
   fprintf(stderr, "\n");
   exit(1);
   
+}
+
+
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // INT
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+
+Node* new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;  
+  return node;
+}
+
+Node* new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node* expr(){
+  Node* node = mul();
+  for(;;){
+    if(consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if(consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+
+Node* mul(){
+  Node *node = primary();
+  for(;;){
+    if(consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if(consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+
+Node* primary(){
+  if(consume('(')){
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
 }
 
 int main(int argc, char **argv) {
